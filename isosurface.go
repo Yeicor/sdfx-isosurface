c9b1c00b0714fb4c8c59578f1c9831f31961a92f
Adrian Cole <adrian@tetrate.io>
2022-11-01 16:01:59 +0800

    deps: updates wazero to 1.0.0-pre.3
    This updates [wazero](https://wazero.io/) to [1.0.0-pre.3](https://github.com/tetratelabs/wazero/releases/tag/v1.0.0-pre.3). Note: wazero 1.0 will require Go 1.18+
    
    Notably, this improves performance and changes host function syntax.
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- isosurface.go
+++ isosurface.go
@@ -34,79 +34,13 @@ func NewRendererCompatible() *Renderer {
 
 func (r *Renderer) Render(sdf3 sdf.SDF3, meshCells int, output chan<- *render.Triangle3) {
 	// Prepare the imports for providing access to our SDF to the code
-	envModule := r.runtime.NewModuleBuilder("env")
-	envModule.ExportFunction("sdf_aabb", func(ctx context.Context, m api.Module) uint32 {
-		box := sdf3.BoundingBox()
-		m.Memory().WriteFloat32Le(ctx, 0, float32(box.Min.X))
-		m.Memory().WriteFloat32Le(ctx, 4, float32(box.Min.Y))
-		m.Memory().WriteFloat32Le(ctx, 8, float32(box.Min.Z))
-		m.Memory().WriteFloat32Le(ctx, 12, float32(box.Max.X))
-		m.Memory().WriteFloat32Le(ctx, 16, float32(box.Max.Y))
-		m.Memory().WriteFloat32Le(ctx, 20, float32(box.Max.Z))
-		return 0
-	})
-	envModule.ExportFunction("sdf_eval", func(ctx context.Context, m api.Module, p uint32) float32 {
-		x, ok := m.Memory().ReadFloat32Le(ctx, p)
-		if !ok {
-			log.Panicln("Read out of range of memory")
-		}
-		y, ok := m.Memory().ReadFloat32Le(ctx, p+4)
-		if !ok {
-			log.Panicln("Read out of range of memory")
-		}
-		z, ok := m.Memory().ReadFloat32Le(ctx, p+8)
-		if !ok {
-			log.Panicln("Read out of range of memory")
-		}
-		return float32(sdf3.Evaluate(sdf.V3{X: float64(x), Y: float64(y), Z: float64(z)}))
-	})
-
-	// Prepare the import for receiving the results
-	envModule.ExportFunction("sdf_mesh_receiver", func(ctx context.Context, m api.Module,
-		verticesPtr uint32, verticesLen uint32, indicesPtr uint32, indicesLen uint32) {
-		//fmt.Println("Got solution with", verticesLen/3, "vertices and", indicesLen, "indices!")
-
-		// Read vertices from memory
-		memIndex := verticesPtr
-		var vertexCoords []float32
-		for i := 0; i < int(verticesLen); i++ {
-			coord, ok := m.Memory().ReadFloat32Le(ctx, memIndex)
-			if !ok {
-				log.Panicln("Read out of range of memory")
-			}
-			vertexCoords = append(vertexCoords, coord)
-			memIndex += 4
-		}
-
-		// Read indices from memory
-		memIndex = indicesPtr
-		var indices []uint32
-		for i := 0; i < int(verticesLen); i++ {
-			coord, ok := m.Memory().ReadUint32Le(ctx, memIndex)
-			if !ok {
-				log.Panicln("Read out of range of memory")
-			}
-			indices = append(indices, coord)
-			memIndex += 4
-		}
-
-		// Give the final triangles back to the user through the channel
-		//fmt.Println("[Sample] Vertices:", vertexCoords[:9], "- Indices:", indices[:3])
-		for faceIndex := 0; faceIndex < len(indices); faceIndex += 3 {
-			index0 := indices[faceIndex]
-			index1 := indices[faceIndex+1]
-			index2 := indices[faceIndex+2]
-			vertex0 := sdf.V3{X: float64(vertexCoords[index0*3]), Y: float64(vertexCoords[index0*3+1]), Z: float64(vertexCoords[index0*3+2])}
-			vertex1 := sdf.V3{X: float64(vertexCoords[index1*3]), Y: float64(vertexCoords[index1*3+1]), Z: float64(vertexCoords[index1*3+2])}
-			vertex2 := sdf.V3{X: float64(vertexCoords[index2*3]), Y: float64(vertexCoords[index2*3+1]), Z: float64(vertexCoords[index2*3+2])}
-			tri := &render.Triangle3{V: [3]sdf.V3{vertex0, vertex1, vertex2}}
-			//if faceIndex < 10 {
-			//	fmt.Println(" - ", tri)
-			//}
-			output <- tri
-		}
-	})
-	_, err := envModule.Instantiate(ctx, r.runtime)
+	h := &host{sdf3: sdf3, output: output}
+	_, err := r.runtime.NewHostModuleBuilder("env").
+		NewFunctionBuilder().WithFunc(h.aabb).Export("sdf_aabb").
+		NewFunctionBuilder().WithFunc(h.eval).Export("sdf_eval").
+		// Prepare the import for receiving the results
+		NewFunctionBuilder().WithFunc(h.meshReceiver).Export("sdf_mesh_receiver").
+		Instantiate(ctx, r.runtime)
 	if err != nil {
 		log.Panicln(err)
 	}
@@ -126,4 +60,79 @@ func (r *Renderer) Render(sdf3 sdf.SDF3, meshCells int, output chan<- *render.Tr
 
 func (r *Renderer) Info(_ sdf.SDF3, meshCells int) string {
 	return "Dual Contouring renderer (meshCells: " + strconv.Itoa(meshCells) + ")"
+}
+
+type host struct {
+	sdf3   sdf.SDF3
+	output chan<- *render.Triangle3
+}
+
+func (h *host) aabb(ctx context.Context, m api.Module) {
+	box := h.sdf3.BoundingBox()
+	m.Memory().WriteFloat32Le(ctx, 0, float32(box.Min.X))
+	m.Memory().WriteFloat32Le(ctx, 4, float32(box.Min.Y))
+	m.Memory().WriteFloat32Le(ctx, 8, float32(box.Min.Z))
+	m.Memory().WriteFloat32Le(ctx, 12, float32(box.Max.X))
+	m.Memory().WriteFloat32Le(ctx, 16, float32(box.Max.Y))
+	m.Memory().WriteFloat32Le(ctx, 20, float32(box.Max.Z))
+}
+
+func (h *host) eval(ctx context.Context, m api.Module, p uint32) float32 {
+	x, ok := m.Memory().ReadFloat32Le(ctx, p)
+	if !ok {
+		log.Panicln("Read out of range of memory")
+	}
+	y, ok := m.Memory().ReadFloat32Le(ctx, p+4)
+	if !ok {
+		log.Panicln("Read out of range of memory")
+	}
+	z, ok := m.Memory().ReadFloat32Le(ctx, p+8)
+	if !ok {
+		log.Panicln("Read out of range of memory")
+	}
+	return float32(h.sdf3.Evaluate(sdf.V3{X: float64(x), Y: float64(y), Z: float64(z)}))
+}
+
+func (h *host) meshReceiver(ctx context.Context, m api.Module, verticesPtr, verticesLen, indicesPtr, indicesLen uint32) {
+	//fmt.Println("Got solution with", verticesLen/3, "vertices and", indicesLen, "indices!")
+
+	// Read vertices from memory
+	memIndex := verticesPtr
+	var vertexCoords []float32
+	for i := 0; i < int(verticesLen); i++ {
+		coord, ok := m.Memory().ReadFloat32Le(ctx, memIndex)
+		if !ok {
+			log.Panicln("Read out of range of memory")
+		}
+		vertexCoords = append(vertexCoords, coord)
+		memIndex += 4
+	}
+
+	// Read indices from memory
+	memIndex = indicesPtr
+	var indices []uint32
+	for i := 0; i < int(indicesLen); i++ {
+		coord, ok := m.Memory().ReadUint32Le(ctx, memIndex)
+		if !ok {
+			log.Panicln("Read out of range of memory")
+		}
+		indices = append(indices, coord)
+		memIndex += 4
+	}
+
+	// Give the final triangles back to the user through the channel
+	//fmt.Println("[Sample] Vertices:", vertexCoords[:9], "- Indices:", indices[:3])
+	for faceIndex := 0; faceIndex < len(indices); faceIndex += 3 {
+		index0 := indices[faceIndex]
+		index1 := indices[faceIndex+1]
+		index2 := indices[faceIndex+2]
+		vertex0 := sdf.V3{X: float64(vertexCoords[index0*3]), Y: float64(vertexCoords[index0*3+1]), Z: float64(vertexCoords[index0*3+2])}
+		vertex1 := sdf.V3{X: float64(vertexCoords[index1*3]), Y: float64(vertexCoords[index1*3+1]), Z: float64(vertexCoords[index1*3+2])}
+		vertex2 := sdf.V3{X: float64(vertexCoords[index2*3]), Y: float64(vertexCoords[index2*3+1]), Z: float64(vertexCoords[index2*3+2])}
+		tri := &render.Triangle3{V: [3]sdf.V3{vertex0, vertex1, vertex2}}
+		//if faceIndex < 10 {
+		//	fmt.Println(" - ", tri)
+		//}
+		h.output <- tri
+	}
 }
